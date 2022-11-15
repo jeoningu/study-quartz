f5562df9c2691dec053398d4e4eef1d97bef4723
jeongingu <doremipasolsol1@gmail.com>
2022-11-16 02:24:49 +0900

    add: quartz를 이용해서 실행할 job 추가, quartz의  jobDetail과 trigger 만드는 함수 추가

---
+++ src/main/java/com/example/studyquartz/info/TimerInfo.java
@@ -0,0 +1,58 @@
+package com.example.studyquartz.info;
+
+public class TimerInfo {
+    private int totalFireCount; // timer가 몇번 실행될 것인지
+    private int remainingFireCount; //남은 실행 횟수?
+    private boolean runForever; // 영원히 실행되게 할건지(영원히 실행되게 할 거라면 totalFierCount는 필요없음)
+    private long repeatIntervalMs; // 몇 Ms 마다 반복할지
+    private long initialOffsetMs; // 0으로 하면 즉시 실행 100으로 하면 100Ms 뒤 실행
+    private String callbackData; //
+
+    public int getTotalFireCount() {
+        return totalFireCount;
+    }
+
+    public void setTotalFireCount(int totalFireCount) {
+        this.totalFireCount = totalFireCount;
+    }
+
+    public int getRemainingFireCount() {
+        return remainingFireCount;
+    }
+
+    public void setRemainingFireCount(int remainingFireCount) {
+        this.remainingFireCount = remainingFireCount;
+    }
+
+    public boolean isRunForever() {
+        return runForever;
+    }
+
+    public void setRunForever(boolean runForever) {
+        this.runForever = runForever;
+    }
+
+    public long getRepeatIntervalMs() {
+        return repeatIntervalMs;
+    }
+
+    public void setRepeatIntervalMs(long repeatIntervalMs) {
+        this.repeatIntervalMs = repeatIntervalMs;
+    }
+
+    public long getInitialOffsetMs() {
+        return initialOffsetMs;
+    }
+
+    public void setInitialOffsetMs(long initialOffsetMs) {
+        this.initialOffsetMs = initialOffsetMs;
+    }
+
+    public String getCallbackData() {
+        return callbackData;
+    }
+
+    public void setCallbackData(String callbackData) {
+        this.callbackData = callbackData;
+    }
+}
---
+++ src/main/java/com/example/studyquartz/jobs/HelloWorldJob.java
@@ -0,0 +1,25 @@
+package com.example.studyquartz.jobs;
+
+import com.example.studyquartz.info.TimerInfo;
+import org.quartz.Job;
+import org.quartz.JobDataMap;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloWorldJob implements Job {
+    private static final Logger LOG = LoggerFactory.getLogger(HelloWorldJob.class);
+
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        JobDataMap jobDataMap = context.getJobDetail().getJobDataMap();
+        TimerInfo info = (TimerInfo) jobDataMap.get(HelloWorldJob.class.getSimpleName());
+
+        //LOG.info("Hello world job start");
+        //LOG.info("callbackData : " + info.getCallbackData());
+        LOG.info("Remaining fire count is '{}' : " + info.getRemainingFireCount());
+    }
+}
