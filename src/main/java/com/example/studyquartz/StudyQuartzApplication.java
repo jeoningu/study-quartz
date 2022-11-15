42aaa1c46babb67813fc4f1146ff3f1ce8644639
jeongingu <doremipasolsol1@gmail.com>
2022-11-16 02:08:05 +0900

    env: Setup development environment
    #Spring Intializr
    java, gradle, jdk11, java11, jar
    
    #Added dependencies
    Quartz Scheduler
    Spring Web

---
+++ src/main/java/com/example/studyquartz/StudyQuartzApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyquartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyQuartzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyQuartzApplication.class, args);
+    }
+
+}
